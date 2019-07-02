a0171eda431c41c93d40985dae35770a276e8505
Austin Webre <austin.webre@gmail.com>
2019-07-02 10:40:33 -0500

    initial commit

---
+++ EffSharpBot/Program.fs
@@ -0,0 +1,20 @@
+﻿open Discord
+
+// Learn more about F# at http://fsharp.org
+
+open Discord.WebSocket
+open System
+open System.Threading.Tasks
+
+let log = Func<LogMessage, Task>(fun (logMessage:LogMessage) -> 
+    Console.WriteLine(logMessage.ToString())
+    Task.CompletedTask)
+
+[<EntryPoint>]
+let main argv =
+    async {
+        let client = new DiscordSocketClient()
+        client.add_Log(log)
+        
+        return 0
+    } |> Async.RunSynchronously
